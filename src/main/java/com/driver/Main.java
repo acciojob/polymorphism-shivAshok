c1f2ef9a366d2d206b4343207d769e19151d4a08
Shiv mishra <sm096951@gmail.com>
2023-06-09 06:06:51 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,21 @@ package com.driver;
 
 public class Main {
 
+ static  class Product{
+        public int product(int x,int y){
+return 1;
+        }
+        public int product(int x,int y,int z){
+            return 2;
+        }
+        public double product(double x,double y){
+            return 3.0;
+        }
+    }
+    public static void main(String[] args){
+        Product P=new Product();
+        P.product(1,2);
+        P.product(1,2,3);
+        P.product(1.2,1.3);
+    }
 }
